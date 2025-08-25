cddb5ce105d0109f38f6f2cf4b31471abafb46ba
Humberto Hernandez <humberto@Humbertos-MacBook-Pro.local>
2025-08-25 09:08:50 -0500

    Added support for Cloud watch and archive

---
+++ factory-example/sertrac/sertrac-event-hub/stack/src/main/kotlin/org/myorg/sertrac/Archive.kt
@@ -0,0 +1,20 @@
+package org.myorg.sertrac
+
+import software.amazon.awscdk.Duration
+import software.amazon.awscdk.services.events.Archive
+import software.amazon.awscdk.services.events.EventPattern
+import software.amazon.awscdk.services.events.Match
+
+private fun MyStack.archiveEvents() {
+    val archive = Archive.Builder
+        .create(this, "Archive")
+        .archiveName("${service()}-${stage()}-archive")
+        .sourceEventBus(myBus)
+        .eventPattern(
+            EventPattern.builder()
+                .detailType(Match.anythingBut("fault"))
+                .build()
+        )
+        .retention(Duration.days(14))
+        .build()
+}
--- factory-example/sertrac/sertrac-event-hub/stack/src/main/kotlin/org/myorg/sertrac/MyStack.kt
+++ factory-example/sertrac/sertrac-event-hub/stack/src/main/kotlin/org/myorg/sertrac/MyStack.kt
@@ -1,28 +1,10 @@
 package org.myorg.sertrac
 
 import software.amazon.awscdk.Duration
-import software.amazon.awscdk.RemovalPolicy
-import software.amazon.awscdk.services.dynamodb.Attribute
-import software.amazon.awscdk.services.dynamodb.AttributeType
-import software.amazon.awscdk.services.dynamodb.StreamViewType
-import software.amazon.awscdk.services.dynamodb.Table
+import software.amazon.awscdk.services.events.Archive
 import software.amazon.awscdk.services.events.EventBus
 import software.amazon.awscdk.services.events.EventPattern
 import software.amazon.awscdk.services.events.Match
-import software.amazon.awscdk.services.events.Rule
-import software.amazon.awscdk.services.events.RuleTargetInput
-import software.amazon.awscdk.services.events.targets.KinesisStream
-import software.amazon.awscdk.services.iam.PolicyDocument
-import software.amazon.awscdk.services.iam.PolicyStatement
-import software.amazon.awscdk.services.iam.Role
-import software.amazon.awscdk.services.iam.ServicePrincipal
-import software.amazon.awscdk.services.kinesis.Stream
-import software.amazon.awscdk.services.kinesis.StreamEncryption
-import software.amazon.awscdk.services.lambda.Code
-import software.amazon.awscdk.services.lambda.Function
-import software.amazon.awscdk.services.lambda.Runtime
-import software.amazon.awscdk.services.lambda.StartingPosition
-import software.amazon.awscdk.services.lambda.eventsources.DynamoEventSource
 import software.constructs.Construct
 
 class MyStack(scope: Construct, serviceProps: ServiceProps) : BaseStack(scope, serviceProps) {
@@ -31,11 +13,15 @@ class MyStack(scope: Construct, serviceProps: ServiceProps) : BaseStack(scope, s
 
     init {
         this.sendEventsToKinesis()
+        this.logEventsInCloudWatch()
+        // this.archiveEvents()
     }
 
     private fun newBus() : EventBus = EventBus.Builder
         .create(this, "Bus")
         .eventBusName("${service()}-${stage()}-bus")
         .build()
+
+
 
 }
