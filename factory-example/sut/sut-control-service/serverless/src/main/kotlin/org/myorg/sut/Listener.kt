faa4859d0486cb5f3b95b9466745c93c0c2c1ae9
Humberto Hernandez <627910+huherto@users.noreply.github.com>
2026-03-10 15:09:18 -0500

    Refactor `Listener` to simplify `Pipeline` initialization, relocate `DynamoDBClientWrapper` to a separate class, and clean up unused code and imports.

--- factory-example/sut/sut-control-service/serverless/src/main/kotlin/org/myorg/sut/Listener.kt
+++ factory-example/sut/sut-control-service/serverless/src/main/kotlin/org/myorg/sut/Listener.kt
@@ -1,35 +1,17 @@
 package org.myorg.sut
 
 import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
-import aws.sdk.kotlin.services.dynamodb.model.PutItemRequest
-import aws.sdk.kotlin.services.dynamodb.model.PutItemResponse
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent
 import io.github.huherto.awsLambdaStream.*
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-import org.slf4j.MarkerFactory
 import java.nio.ByteBuffer
 
 class MyKinesisAdapter : KinesisAdapter() {
     override fun decodePayload(payload: ByteBuffer?): TrackedUnitEvent {
         return sutJson.decodeFromString<TrackedUnitEvent>(utf8Decode(payload))
-    }
-}
-
-class DynamoDBClientWrapper(val dynamoDBClient: DynamoDbClient) : DynamoDbClient by dynamoDBClient {
-
-    private val logger = KotlinLogging.logger {  }
-
-    override suspend fun putItem(input: PutItemRequest): PutItemResponse {
-        val eventId = input.item?.get("pk")?.asS()
-        logger.info { "Checking for poison event: $eventId" }
-        if (eventId?.contains("poison") == true) {
-            throw RuntimeException("Poison event detected $eventId")
-        }
-        logger.info { "Putting item: $eventId" }
-        return dynamoDBClient.putItem(input)
     }
 }
 
@@ -48,35 +30,28 @@ class Listener(
         }
     }
 
-    private val assembler: PipelineAssembler by lazy {
-
+    private val pipeline: Pipeline by lazy {
         if (dynamoDbClient == null) {
             dynamoDbClient = DynamoDBClientWrapper(getDynamoDbClient(envConfig))
         }
 
-        val pipeline =
-            CollectPipeline.Builder("col1")
-                .dynamoDbClient(dynamoDbClient)
-                .onEventClass(listOf(Event::class))
-                .envConfig(envConfig)
-                .build()
-        val assembler = PipelineAssembler
-            .builder()
-                .addPipeline(pipeline)
-                .build()
-        assembler
+        CollectPipeline.Builder("collect1")
+            .dynamoDbClient(dynamoDbClient)
+            .onEventClass(listOf(Event::class))
+            .envConfig(envConfig)
+            .build()
     }
 
     override fun handleRequest(kinesisEvent: KinesisEvent, context: Context): Void? = runBlocking{
-        // Is this try..catch even needed any more?
-        try {
-            val headFlow = kinesisAdapter.fromKinesis(kinesisEvent)
-            assembler
-                .assemble(headFlow, true)
-                .collect { logger.info { "collected " + it.event?.id} }
-        } catch (e: Throwable) {
-            logger.error(MarkerFactory.getMarker("FATAL"), "Exception in lambda handler", e)
-        }
+        val assembler = PipelineAssembler
+            .builder()
+            .addPipeline(pipeline)
+            .build()
+
+        val headFlow = kinesisAdapter.fromKinesis(kinesisEvent)
+        assembler
+            .assemble(headFlow, true)
+            .collect { logger.info { "collected " + it.event?.id} }
         null
     }
 }
