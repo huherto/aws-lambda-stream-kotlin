e61e9eacf8438cb9cbdae3fdb2c099bcaf1a0099
Humberto Hernandez <627910+huherto@users.noreply.github.com>
2026-06-26 12:58:50 -0500

    Work in progress. Implementing regional health check lambda functions.

---
+++ examples/sut/sut-regional-health-check/app/src/main/kotlin/org/myorg/sut/Connector.kt
@@ -0,0 +1,75 @@
+package org.myorg.sut
+
+import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
+import aws.sdk.kotlin.services.dynamodb.model.*
+import io.github.huherto.awsLambdaStream.sinks.DynamoDbUpdateValue
+import io.github.huherto.awsLambdaStream.sinks.updateExpression
+import kotlin.time.Duration.Companion.milliseconds
+
+class Connector(
+    debug: (String) -> Unit,
+    tableName: String?,
+    timeout: Long = System.getenv("DYNAMODB_TIMEOUT")?.toLongOrNull()
+        ?: System.getenv("TIMEOUT")?.toLongOrNull()
+        ?: 1_000L,
+) {
+    private val debug: (Any?) -> Unit = { msg -> debug(msg.toString()) }
+    private val tableName: String = tableName ?: "undefined"
+
+    private val db: aws.sdk.kotlin.services.dynamodb.DynamoDbClient = DynamoDbClient {
+        this.clientName = "Connector"
+        this.httpClient {
+            this.socketReadTimeout = timeout.milliseconds
+        }
+    }
+
+    suspend fun update(
+        key: Map<String, AttributeValue>,
+        inputParams: Map<String, DynamoDbUpdateValue>,
+    ): UpdateItemResponse {
+        val expression = updateExpression(inputParams)
+
+        val request = UpdateItemRequest {
+            tableName = this@Connector.tableName
+            this.key = key
+            expressionAttributeNames = expression.expressionAttributeNames
+            expressionAttributeValues = expression.expressionAttributeValues
+            updateExpression = expression.updateExpression
+            returnValues = ReturnValue.AllNew
+        }
+
+        return runCatching {
+            db.updateItem(request)
+        }.onSuccess {
+            debug(it)
+        }.onFailure {
+            debug(it)
+        }.getOrThrow()
+    }
+
+    suspend fun get(id: String): List<Map<String, AttributeValue>> {
+        val request = QueryRequest {
+            tableName = this@Connector.tableName
+            limit = 3
+            scanIndexForward = false
+            keyConditionExpression = "#pk = :pk"
+            expressionAttributeNames = mapOf(
+                "#pk" to "pk",
+            )
+            expressionAttributeValues = mapOf(
+                ":pk" to AttributeValue.S(id),
+            )
+            consistentRead = true
+        }
+
+        return runCatching {
+            db.query(request)
+        }.onSuccess {
+            debug(it)
+        }.onFailure {
+            debug(it)
+        }.getOrThrow()
+            .items
+            .orEmpty()
+    }
+}
