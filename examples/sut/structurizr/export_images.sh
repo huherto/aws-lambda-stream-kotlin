a96443588dbb5a6dfafd1a7861062318c4eafa36
Humberto Hernandez <627910+huherto@users.noreply.github.com>
2026-09-14 09:24:14 -0500

    Add script to export Structurizr diagrams; ensure `structurizr-preview.war` is available

--- examples/sut/structurizr/export_images.sh
+++ examples/sut/structurizr/export_images.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
 
+# Calculate directory where this script is located.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+if [ ! -e structurizr-preview.war ]
+then
+  echo "Please download structurizr-preview.war"
+  echo "curl -O https://download.structurizr.com/structurizr-preview.war"
+  exit 1
+fi
 
 java -jar structurizr-preview.war export --workspace workspace.json --format svg --url http://localhost:8080/workspace/1/diagrams --output ../images
